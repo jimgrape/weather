41fc8eed7ae528f9b0e46d6a07506ac1fbb2c0d4
ksy <910661511@163.com>
2018-04-10 14:02:24 +0800

    fix bug

--- weather.py
+++ weather.py
@@ -12,7 +12,7 @@ def get_content(city_code):
 # 获取关键数据
 def get(html):
     # 日期、相对天数、天气情况、高温、低温、风向、风级
-    reg = re.compile(r'li class="sky skyid lv.*?<h1>(.*?)（(.*?)）</h1>.*?<p title="(.*?)".*?<span>(.*?)</span>.*?<i>(.*?)℃</i>.*?<span title="(.*?)".*?</em>\n<i>(.*?)</i>', re.S)
+    reg = re.compile(r'li class="sky skyid lv.*?<h1>(.*?)（(.*?)）</h1>.*?<p title="(.*?)".*?<span>(.*?)℃</span>.*?<i>(.*?)℃</i>.*?<span title="(.*?)".*?</em>\n<i>(.*?)</i>', re.S)
     items = re.findall(reg, html)
     return items
 
